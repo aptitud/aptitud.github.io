26ad2342d6be8c89d0faed7092cc6d18be21b122
Marcus Nylander <marcus.nylander@aptitud.se>
2015-01-30 13:36:47 +0100

    Added handlebars example for fellows

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+$(function () {
+    var page = window.location.hash.replace("#",'');
+    if (page === "" || page === undefined) { // TODO: Check valid pages and have a default
+        page = "start"
+    }
+    $("#includedContent").load(page + ".html");
+});
+
 function fellow(img, name, desc) {
     return {
         img: img,
@@ -11,9 +19,8 @@ var fellows = [
     fellow('profile_pic.png', 'Firstname3 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.')
 ];
 
-var fellowTemplate = Handlebars.compile($("fellow-template").html());
-
 function layoutFellows() {
+    var fellowTemplate = Handlebars.compile($("fellow-template").html());
     var fellowList = "";
     fellows.forEach(function (f) {
         fellowList += fellowTemplate(f);
--- script.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    var page = window.location.hash.replace("#",'');
-    if (page === "" || page === undefined) { // TODO: Check valid pages and have a default
-        page = "start"
-    }
-    $("#includedContent").load(page + ".html");
-});
