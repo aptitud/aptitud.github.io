361321cee9f49f8f67e77b204b3ce98126c65abd
Marcus Nylander <marcus.nylander@aptitud.se>
2015-01-30 14:22:23 +0100

    Fixed fellow page navigation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 $(function () {
-    var page = window.location.hash.replace("#",'');
+    routeWithHash();
+    $(".nav a").on('click', function(e) {
+        console.log(e);
+        routeTo($(e.target).attr('href').replace("#", ''));
+    });
+});
+
+function routeWithHash() {
+    routeTo(window.location.hash.replace("#", ''));
+}
+
+function routeTo(page) {
     if (page === "" || page === undefined) { // TODO: Check valid pages and have a default
         page = "start"
     }
-    $("#includedContent").load(page + ".html");
-});
+    window.page = undefined;
+    $("#includedContent").load(page + ".html", function() {
+        // on completed call init in file
+        if (window.page && window.page.init) {
+            window.page.init();
+        }
+    });
+}
 
 function fellow(img, name, desc) {
     return {
@@ -16,14 +33,19 @@ function fellow(img, name, desc) {
 var fellows = [
     fellow('profile_pic.png', 'Firstname1 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
     fellow('profile_pic.png', 'Firstname2 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
-    fellow('profile_pic.png', 'Firstname3 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.')
+    fellow('profile_pic.png', 'Firstname3 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
+    fellow('profile_pic.png', 'Firstname4 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
+    fellow('profile_pic.png', 'Firstname5 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
+    fellow('profile_pic.png', 'Firstname6 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
+    fellow('profile_pic.png', 'Firstname7 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
+    fellow('profile_pic.png', 'Firstname8 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
+    fellow('profile_pic.png', 'Firstname9 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.')
 ];
 
 function layoutFellows() {
-    var fellowTemplate = Handlebars.compile($("fellow-template").html());
-    var fellowList = "";
+    var fellowTemplate = Handlebars.compile($("#fellow-template").html());
+    var layoutDiv = $("#fellow-list");
     fellows.forEach(function (f) {
-        fellowList += fellowTemplate(f);
+        layoutDiv.append(fellowTemplate(f));
     });
-    $("#fellow-layout").html(f);
 }
