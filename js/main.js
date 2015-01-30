b9c10fde2eac0b1095ae1bcc30951b22cf4b2b32
Marcus Nylander <marcus.nylander@aptitud.se>
2015-01-30 14:54:47 +0100

    Added other pages

--- js/main.js
+++ js/main.js
@@ -21,30 +21,3 @@ function routeTo(page) {
         }
     });
 }
-
-function fellow(img, name, desc) {
-    return {
-        img: img,
-        name: name,
-        desc: desc
-    };
-}
-var fellows = [
-    fellow('profile_pic.png', 'Firstname1 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
-    fellow('profile_pic.png', 'Firstname2 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
-    fellow('profile_pic.png', 'Firstname3 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
-    fellow('profile_pic.png', 'Firstname4 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
-    fellow('profile_pic.png', 'Firstname5 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
-    fellow('profile_pic.png', 'Firstname6 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
-    fellow('profile_pic.png', 'Firstname7 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
-    fellow('profile_pic.png', 'Firstname8 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.'),
-    fellow('profile_pic.png', 'Firstname9 Lastname', 'Bacon ipsum dolor amet brisket boudin fatback, tri-tip corned beef turducken sausage balltip andouille. Turducken tri-tip swine filet mignon.')
-];
-
-function layoutFellows() {
-    var fellowTemplate = Handlebars.compile($("#fellow-template").html());
-    var layoutDiv = $("#fellow-list");
-    fellows.forEach(function (f) {
-        layoutDiv.append(fellowTemplate(f));
-    });
-}
