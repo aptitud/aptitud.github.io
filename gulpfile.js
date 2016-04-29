71accdb65ff6734dd5e283e2a02fbfcf6586ab16
Per Jansson <per.r.jansson@gmail.com>
2016-04-29 20:34:51 +0200

    Clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,9 @@
 var gulp = require('gulp'),
-  requireDir = require('require-dir'),
   connect = require('gulp-connect'),
   runSequence = require('run-sequence'),
   sass = require('gulp-sass');
 
 gulp.task('default', ['connect', 'watch']);
-
-gulp.task('sass', function() {
-  return gulp.src('./src/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/css'));
-});
 
 gulp.task('connect', function() {
   connect.server({
@@ -19,15 +12,21 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('reload', function(callback) {
-  runSequence('sass', 'resources', callback);
-});
-
-gulp.task('resources', function() {
-  gulp.src('index.html')
-    .pipe(connect.reload());
-});
-
 gulp.task('watch', function() {
   gulp.watch(['index.html', './src/**/*.**'], ['reload']);
+});
+
+gulp.task('reload', function(callback) {
+  runSequence('sass', 'reload-resources', callback);
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('reload-resources', function() {
+  gulp.src('index.html')
+    .pipe(connect.reload());
 });
