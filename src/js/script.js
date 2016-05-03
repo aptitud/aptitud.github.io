07ae22e73bbe9091db0d796e234f018866673456
Magnus Hübner <magnus.hubner@aptitud.se>
2016-05-04 00:14:15 +0200

    Fixed styling/layout and expand functionality. Added contract pane functionality.

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   $('section').on('click', '.expand', function(e) {
-    $(this).closest('section').addClass('expanded');
+    var $section = $(this).closest('section');
+    $section.css('max-height',$section[0].scrollHeight);
+    $section.addClass('expanded');
+  })
+  $('section').on('click', '.contract', function(e) {
+    var $section = $(this).closest('section');
+    $section.prop('style','');
+    $section.removeClass('expanded');
   })
 });
