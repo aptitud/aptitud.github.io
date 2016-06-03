9a1639a724ffd71386af19c9759a372373364a9f
Mikael Ahlinder <mikael.ahlinder@aptitud.se>
2016-06-03 09:50:41 +0200

    stuff4

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
+  var maxheight = '40vh';
   $('section').on('click', '.expand', function(e) {
     var $section = $(this).closest('section');
-    $section.data('max-height', $section.css('max-height'));
-    console.log($section.css('max-height'));
     $section.css('max-height', $section[0].scrollHeight);
     $section.addClass('expanded');
   })
   $('section').on('click', '.contract', function(e) {
     var $section = $(this).closest('section');
-    $section.css('max-height', $section.data('init-height'));
+    $section.css('max-height', maxheight);
     $section.removeClass('expanded');
   })
 });
