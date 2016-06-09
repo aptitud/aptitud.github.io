9b9f3a018c955000043c8b38d30716954e51b925
Magnus Hübner <magnus.hubner@aptitud.se>
2016-06-09 21:29:14 +0200

    Changed content. Changed layouts. Changed everything. Almost.

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,16 @@
-$(document).ready(function() {
-  var maxheight = '40vh';
-  $('section').on('click', '.expand', function(e) {
-    var $section = $(this).closest('section');
-    $section.css('max-height', $section[0].scrollHeight);
-    $section.addClass('expanded');
-  })
-  $('section').on('click', '.contract', function(e) {
-    var $section = $(this).closest('section');
-    $section.css('max-height', maxheight);
-    $section.removeClass('expanded');
-  })
+$(document).ready(function () {
+	$('section').on('click', '.expand', function (e) {
+		var $section = $(this).closest('section');
+		if ($section.length) {
+			$section.data('collapsed-height', $section.outerHeight()).css('max-height', $section[0].scrollHeight).addClass('expanded');
+		}
+	});
+	$('section').on('click', '.contract', function (e) {
+		var $section = $(this).closest('section');
+		if ($section.length) {
+			$section.css('max-height', $section.data('collapsed-height')).removeClass('expanded');
+		}
+	});
+	$(".button-collapse").sideNav();
+//	$('.parallax').parallax();
 });
