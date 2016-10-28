8e52b5c9077b5d6f2d7b5d3716d40f985d4ded30
Magnus Hübner <magnus.hubner@aptitud.se>
2016-10-28 16:19:34 +0200

    scroll to and open section when using menu

--- src/js/script.js
+++ src/js/script.js
@@ -3,20 +3,34 @@ var initPushpinMenu = function() {
 	$('.navbar nav').pushpin({ top: $('#logo').outerHeight() });
 }
 
+var expandSection = function (e) {
+	var $section = $(this).closest('section');
+	if ($section.length) {
+		$section.data('collapsed-height', $section.outerHeight()).css('max-height', $section[0].scrollHeight).addClass('expanded');
+	}
+}
+
+var contractSection = function (e) {
+	var $section = $(this).closest('section');
+	if ($section.length) {
+		$section.css('max-height', $section.data('collapsed-height')).removeClass('expanded');
+	}
+}
+
+var navToSection = function (e) {
+	e.preventDefault();
+	var $section = $($(this).attr('href'));
+	console.log($(this).attr('href'));
+	$('html,body').animate({scrollTop: $section.offset().top-66},'slow');
+	$section.not('.expanded').data('collapsed-height', $section.outerHeight()).css('max-height', $section[0].scrollHeight).addClass('expanded');
+}
+
 $(document).ready(function () {
-	$('section').on('click', '.expand', function (e) {
-		var $section = $(this).closest('section');
-		if ($section.length) {
-			$section.data('collapsed-height', $section.outerHeight()).css('max-height', $section[0].scrollHeight).addClass('expanded');
-		}
-	});
-	$('section').on('click', '.contract', function (e) {
-		var $section = $(this).closest('section');
-		if ($section.length) {
-			$section.css('max-height', $section.data('collapsed-height')).removeClass('expanded');
-		}
-	});
+	$('section').on('click', '.expand', expandSection);
+	$('section').on('click', '.contract', contractSection);
 	$('.button-collapse').sideNav({closeOnClick:true});
+
+	$('.navbar').on('click', 'li a', navToSection);
 
 	setTimeout(initPushpinMenu,1000);
 });
